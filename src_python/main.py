a727c81858fe071e0481372d63fa3c560d722b4f
Malkrich <mathurin.lemarie@gmail.com>
2022-04-06 09:09:22 +0200

    ajout traitement B&W + seuillage

--- src_python/main.py
+++ src_python/main.py
@@ -1,19 +1,27 @@
 import numpy as np
 import cv2
+import imutils
 
+    
 
+# lancement capture vidéo
 cap = cv2.VideoCapture(0)
 
+# lecture frame by frame
 while(True):
     # Capture frame by frame
     ret, frame = cap.read()
     
     # opérations
-    
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    blurred = cv2.GaussianBlur(gray, (11, 11), 0)
+    thresh = cv2.threshold(blurred, 70, 255, cv2.THRESH_BINARY)[1]
+    cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
     
     # Display
-    cv2.imshow('frame',frame)
-    cv2.imshow('gray',gray)
+    cv2.imshow('frame',thresh)
+    #cv2.imshow('gray',gray)
     
     # on appuis sur q pour quitter
     if cv2.waitKey(20) & 0xFF == ord('q'):
