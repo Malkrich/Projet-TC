1fa9ae6fd0c2efc477ee8002476c827c3fdaa663
Malkrich <mathurin.lemarie@gmail.com>
2022-04-06 09:20:42 +0200

    ajout fonction shape_detection

--- src_python/main.py
+++ src_python/main.py
@@ -2,6 +2,11 @@ import numpy as np
 import cv2
 import imutils
 
+def shape_detection(img):
+    cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
+    
+    return cnts
     
 
 # lancement capture vidéo
@@ -13,15 +18,13 @@ while(True):
     ret, frame = cap.read()
     
     # opérations
+    # pre traitement
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     blurred = cv2.GaussianBlur(gray, (11, 11), 0)
     thresh = cv2.threshold(blurred, 70, 255, cv2.THRESH_BINARY)[1]
-    cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
-    cnts = imutils.grab_contours(cnts)
     
     # Display
     cv2.imshow('frame',thresh)
-    #cv2.imshow('gray',gray)
     
     # on appuis sur q pour quitter
     if cv2.waitKey(20) & 0xFF == ord('q'):
