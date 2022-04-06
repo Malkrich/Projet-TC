732089a6288c2c98f569306b2938d32ea1dcc56e
Malkrich <mathurin.lemarie@gmail.com>
2022-04-06 11:29:37 +0200

    rough contours

--- src_python/main.py
+++ src_python/main.py
@@ -3,8 +3,9 @@ import cv2
 import imutils
 
 def shape_detection(img):
-    cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
-    cnts = imutils.grab_contours(cnts)
+    cnts, hierarchy = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+    
+    #cnts = imutils.grab_contours(cnts)
     
     return cnts
     
@@ -20,11 +21,12 @@ while(True):
     # opérations
     # pre traitement
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    blurred = cv2.GaussianBlur(gray, (11, 11), 0)
-    thresh = cv2.threshold(blurred, 70, 255, cv2.THRESH_BINARY)[1]
+    ret, thresh = cv2.threshold(gray, 127, 255, 0)
     
-    # Display
-    cv2.imshow('frame',thresh)
+    cnts = shape_detection(thresh)
+    cv2.drawContours(frame, cnts, -1, (0,255,0), 3)
+    cv2.imshow('contours',frame)
+    
     
     # on appuis sur q pour quitter
     if cv2.waitKey(20) & 0xFF == ord('q'):
