cda1c5a17f203503c9c8003b6c5acf4cdc3b5088
Malkrich <mathurin.lemarie@gmail.com>
2022-04-26 09:35:15 +0200

    changement attributs class_form

--- src_python/class_form.py
+++ src_python/class_form.py
@@ -1,7 +1,9 @@
 
 
 class form:
-    poly = {
+    sommets = 0
+    pol = 'undefined'
+    collec_poly = {
         3 : 'triangle',
         4 : 'rectangle',
         5 : 'pentagone',
@@ -9,12 +11,15 @@ class form:
         }
     
     def __init__(self):
-        self.sommets = 0
+        print('ajout instance class form')
     
     def set_sommets(self,s):
         self.sommets = s
+        self.get_form()
     
+    def get_form(self):
+        self.pol = self.collec_poly.get(self.sommets,'undefined')
         
     # fonction de test
     def display_info(self):
-        print(self.poly.get(self.sommets,'undefined'))
+        print(self.pol)
