0890d0de62bf40cf8ac21ef0ef35d1380cf7431f
Malkrich <mathurin.lemarie@gmail.com>
2022-04-27 09:32:58 +0200

    ajout affichage de la carte binaire pour debug

--- src_python/detect_cible.py
+++ src_python/detect_cible.py
@@ -2,17 +2,15 @@ import numpy as np
 import cv2
 import imutils
 
-from class_form import form
-
 
 def get_red(img):
     """ Solution RGB """
     blue_c = img[:,:,0]
     green_c = img[:,:,1]
     red_c = img[:,:,2]
-    ret_b, blue = cv2.threshold(blue_c,190,255,0)
-    ret_g, green = cv2.threshold(green_c,190,255,0)
-    ret_r, red = cv2.threshold(red_c,245,255,0)
+    ret_b, blue = cv2.threshold(blue_c,150,255,0)
+    ret_g, green = cv2.threshold(green_c,150,255,0)
+    ret_r, red = cv2.threshold(red_c,240,255,0)
 
     # on fait : /(blue && green) && red
     color_to_detect = red
@@ -26,6 +24,9 @@ def get_red(img):
     kernel = None
     erosion = cv2.erode(result,kernel,iterations=2)
     dilatation = cv2.dilate(erosion,kernel,iterations=2)
+    
+    # debug
+    #cv2.imshow('dilatation',dilatation)
     
     """ Solution HSV """
     #hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
@@ -41,40 +42,12 @@ def contours_detection(img):
     cnts = imutils.grab_contours(cnts)
     
     return cnts
-    
 
-# lancement capture vidéo
-cap = cv2.VideoCapture(0)
-
-f = form()
-
-# lecture frame par frame
-while(True):
-    # Capture frame by frame
-    ret, frame = cap.read()
-    
+def main_contour(frame):    
     # pre traitement
     logical_img = get_red(frame)
     
     # detection de contours
     cnts = contours_detection(logical_img)
     
-    for c in cnts:
-        peri = cv2.arcLength(c, True)
-        approx = cv2.approxPolyDP(c, 0.04 * peri, True)
-        
-        n = np.sqrt(np.sum(np.power(approx,2)))
-        if n > 1000:
-            cv2.drawContours(frame, [approx], -1, (0,255,0), 3)
-            f.set_forme(len(approx))
-            f.display_info()
-    
-    cv2.imshow('contours',frame)
-    
-    
-    # on appuis sur q pour quitter
-    if cv2.waitKey(20) & 0xFF == ord('q'):
-        break
-    
-cap.release()
-cv2.destroyAllWindows()
+    return cnts
