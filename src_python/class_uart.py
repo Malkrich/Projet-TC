a8de714bcaed69190b7b85695f4763a5810dc26b
Malkrich <mathurin.lemarie@gmail.com>
2022-05-04 10:51:57 +0200

    ajout class uart

---
+++ src_python/class_uart.py
@@ -0,0 +1,23 @@
+"""
+Tram :
+    Préfixe :
+        - M : servomoteur
+        - L : lampe
+    
+    Moteurs :
+    
+    Lampe :
+        1) H/B : haut/bas
+        2) G/D/C : gauche/droite/centre
+
+    En fin de chaine : +"X"
+"""
+
+
+class uart:
+    commande = ""
+    
+    def __init__(self):
+        print("ajout instance class uart")
+        
+    
