85d49dab56d5092af36f9f992d9dfaa4ddfb5351
Malkrich <mathurin.lemarie@gmail.com>
2022-05-04 12:06:23 +0200

    test zones

--- src_python/tests_image.py
+++ src_python/tests_image.py
@@ -1,6 +1,8 @@
 import cv2
 import numpy as np
 
+from class_zone import zone_HG,zone_HD,zone_BG,zone_BD,zone_C
+
 cap = cv2.VideoCapture(0)
 
 # calcul hauteur, largeur
@@ -10,9 +12,24 @@ w = len(frame[0])
 l = (int)(w/3)
 eps = (int)(h/3)
 
-cnts = [[0,0],[w/2,0],[w/2,h/2],[0,h/2]]
+## class zones
+haut_gauche = zone_HG(h,w,l,eps)
+haut_droite = zone_HD(h,w,l,eps)
+bas_gauche = zone_BG(h,w,l,eps)
+bas_droite = zone_BD(h,w,l,eps)
+centre = zone_C(h,w,l,eps)
+liste_zone = [centre,haut_gauche,haut_droite,bas_gauche,bas_droite]
+x = 400
+y = 500
+
+for z in liste_zone:
+    if z.in_zone(x,y):
+        print("Le point est dans la zone : ",z.get_name(),", Nb : ",z.get_zoneNb())
+        break
+
+print("Test zone_HG : ",haut_gauche.in_zone(400,500))
+
 rectangle_color=(0,0,255)
-#cv2.drawContours(frame, [cnts], -1, (0,255,0), 3)
 cv2.rectangle(frame,(0,0),((int)(w/2),(int)(h/2)),rectangle_color,2)
 cv2.rectangle(frame,((int)(w/2),0),(w,(int)(h/2)),rectangle_color,2)
 cv2.rectangle(frame,(0,(int)(h/2)),((int)(w/2),(int)(h/2)),rectangle_color,2)
